abe3573ad7aa55227bc772d0a782e0f1e66ee9c7
Hiroki Horiuchi <horiuchi.g@gmail.com>
2021-08-26 13:35:57 +0900

    fix: eslint config

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
